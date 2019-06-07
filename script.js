3cdf96431c58ee3bfb1128bd96ca080f250eab98
varFox <31412438+varFox@users.noreply.github.com>
2019-06-07 19:26:08 +0300

    2

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const slider = (slide, pr, nt, sliderDots, dot) => {
   let slideIndex = 1,
     slideFocus = slides.length;
 
-  const showSlides = (m, n) => {
+  const showSlides = (m, n, prev) => {
     if (n > slides.length) {
       slideIndex = 1;
     }
@@ -26,17 +26,26 @@ const slider = (slide, pr, nt, sliderDots, dot) => {
       item.style.display = 'none'
       item.classList.remove('next-slide-in');
       item.classList.remove('next-slide-out');
+      item.classList.remove('prev-slide-in');
+      item.classList.remove('prev-slide-out');
     });
 
     dots[slideIndex - 1].classList.add('dot-active');
-    slides[slideIndex - 1].classList.add('next-slide-in');
-    slides[slideFocus - 1].classList.add('next-slide-out');
-    slides[slideIndex - 1].style.display = '';
-  }
-  showSlides(slideIndex, slideFocus);
+    if (prev) {
+      slides[slideIndex - 1].classList.add('prev-slide-in');
+      slides[slideFocus - 1].classList.add('prev-slide-out');      
+    } else {
+      slides[slideIndex - 1].classList.add('next-slide-in');
+      slides[slideFocus - 1].classList.add('next-slide-out');      
+    }
 
-  const plusSlides = n => {
-    showSlides(slideFocus = slideIndex, slideIndex += n);
+    slides[slideIndex - 1].style.display = '';
+    slides[slideFocus - 1].style.display = '';
+  }
+  showSlides(slideFocus, slideIndex);
+
+  const plusSlides = (n, prev) => {
+    showSlides(slideFocus = slideIndex, slideIndex += n, prev);
   }
 
   const currentSlide = n => {
@@ -44,7 +53,7 @@ const slider = (slide, pr, nt, sliderDots, dot) => {
   }
 
   prev.addEventListener('click', () => {
-    plusSlides(-1);
+    plusSlides(-1, true);
   });
   next.addEventListener('click', () => {
     plusSlides(1);
